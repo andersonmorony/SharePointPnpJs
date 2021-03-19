6e48595a1e95b43ea097699b843db58bee1877d6
Anderson Moroni <31922891+andersonmorony@users.noreply.github.com>
2021-03-19 10:21:18 -0300

    Add files via upload

---
+++ Style Library/Custom/MyApp/app.js
@@ -0,0 +1,139 @@
+$(document).ready(function(){
+
+    getItem();
+    getLookup();
+    
+
+    $("#btn-salvar").click(function(){
+
+        var title = $("#Title").val();
+
+        addItem({
+            Title: title
+        });
+    });
+    
+    $("#btn-edit").click(function(){
+        
+        editItem({
+            Id: $("#idItem").val(),
+            Title: $("#TitleEdit").val(),
+            idLookup: $("#lookup").val()
+        });
+    });
+
+    $(document).on("click", "#btn-get" ,function(){
+        
+        var Id = $(this).data("id");
+
+        getItemById(Id);
+        
+    });
+
+    $(document).on("click", "#btn-delete" ,function(){
+        
+        var Id = $(this).data("id");
+
+        deleteItem(Id);
+        
+    });
+
+});
+
+function getItemById(id){
+    $pnp.sp.web.lists.getByTitle("Atividades").items.getById(id).get().then(function(res){
+        
+        $("#idItem").val(res.Id);
+        $("#TitleEdit").val(res.Title);
+
+    }).catch(function(err){
+        console.log(err);
+
+    });
+}
+
+function getItem(){
+
+    const camlquery = {
+        ViewXml: "<View><ViewFields><FieldRef Name='ID' /><FieldRef Name='Title' /><FieldRef Name='Lista' /></ViewFields><Query><OrderBy><FieldRef Name='Title' /></OrderBy></Query><RowLimit>10</RowLimit></View>"
+    }
+    
+    $pnp.sp.web.lists.getByTitle("Atividades")
+    .renderListDataAsStream(camlquery)
+    // .getItemsByCAMLQuery(camlquery, "FieldValuesAsText")
+    // .items
+    // .select("Title", "Id", "Lista/Title", "Lista/Id")
+    // // .filter("Title eq 'Anderson'")
+    // .expand("Lista")
+    // .top(5)
+    // .get()
+    .then(function(res){
+
+        console.log(res);
+        
+        var html;
+
+        res.map(function(value){
+        
+            html += `<tr><td>${value.Id}</td><td>${value.Title}</td>           
+            <td>${value.Lista === undefined ? "" : value.Lista.Title}</td> 
+            <td>
+            <button type="button" id="btn-get" data-id="${value.Id}" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                Edit
+            </button>
+            <button type="button" class="btn btn-danger" data-id="${value.Id}" id="btn-delete">Delete</button>
+
+            </td>
+            </tr>`;
+
+        })
+
+        $("#mytable").html(html);
+
+
+
+    }).catch(function(err){
+        console.log(err)
+    })
+}
+
+function addItem(item){
+    $pnp.sp.web.lists.getByTitle("Atividades").items.add({
+        Title: item.Title
+    }).then(function(res){
+        getItem();
+    });
+}
+
+function editItem(item){
+    $pnp.sp.web.lists.getByTitle("Atividades").items.getById(item.Id).update({
+        Title: item.Title,
+        ListaId: item.idLookup
+    }).then(function(resp){
+        getItem();
+    }).catch(function(err){
+        console.log(err);
+    })
+}
+
+function deleteItem(id){
+    $pnp.sp.web.lists.getByTitle("Atividades").items.getById(id).delete().then(function(res){
+        console.log(res);
+        alert("Item deletado");
+        getItem();
+    }).catch(function(err){
+        console.log(err);
+    })
+}
+
+function getLookup(){
+    $pnp.sp.web.lists.getByTitle("MinhaLista").items.getAll().then(function(res){
+
+
+        res.map(function(item){
+            $("#lookup").append(`<option value="${item.Id}">${item.Title}</option>`)
+        })
+
+
+    });
+}
